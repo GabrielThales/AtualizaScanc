44b9f3beaf64835da53ed362118236456fc224bd
gabriel.thales <gabriel.thales@IPRJONW132510>
2024-12-30 18:46:30 -0300

    Adicionar arquivos de projeto.

---
+++ AtualizaScanc/Program.cs
@@ -0,0 +1,31 @@
+﻿using AtualizaScanc;
+using AtualizaScanc.Domain;
+
+Console.WriteLine("AtualizaScanc");
+
+ScancServices atualizaScanc = new ScancServices();
+
+Console.WriteLine("1 - Atualizar Scanc");
+string opcao = Console.ReadLine();
+
+FilialScanc filialScanc = new FilialScanc(Guid.NewGuid(),"SCANC MALUCO", "C:\\TEMP\\scancShortcut.lnk", "C:\\TEMP\\SCANC");
+
+switch (opcao)
+{
+    case "1": Console.WriteLine("Atualizando Scanc");
+
+        atualizaScanc.AtualizaScanc(filialScanc, "C:\\TEMP\\instalar_scanc_ctb.exe");
+        
+        break;
+
+    case "2": Console.WriteLine("Retificadora");
+
+        atualizaScanc.CriaRetificadora(filialScanc.PathInstall,filialScanc, "c:\\temp\\retif2\\SCANC301224", true);
+
+        break;
+
+
+
+
+    default: Console.WriteLine("Opção não encontrada");break;
+}
---
+++ AtualizaScanc/ScancServices.cs
@@ -0,0 +1,117 @@
+﻿using AtualizaScanc.Domain;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime;
+using IWshRuntimeLibrary;
+
+namespace AtualizaScanc
+{
+    internal class ScancServices
+    {
+
+        public void AtualizaScanc(FilialScanc filial, String pathInstaller)
+        {
+
+
+            try
+            {
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = pathInstaller,
+                    UseShellExecute = true,
+                    Arguments = " /DIR=" + '"' + filial.PathInstall + '"' + " /SILENT"
+                };
+                
+                
+                //Process.Start(pathInstaller);
+
+
+                Process.Start(psi);
+                CriaAtalho(filial);
+
+            }
+            catch (Exception ex) { 
+            
+                Console.WriteLine("Algo deu errado:  " + ex.ToString());
+            
+            }
+
+        }
+
+        public void CriaAtalho(FilialScanc filial)
+        {
+
+            try
+            {
+                if (!System.IO.File.Exists(filial.PathShortcut))
+                {
+                    WshShell shell = new WshShell();
+                    IWshShortcut link = shell.CreateShortcut(filial.PathShortcut);
+                    link.TargetPath = filial.PathInstall + "\\BIN\\SCANCCTB.EXE";
+                    link.Save();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.ToString());
+
+            }
+
+        }
+
+        public void CriaRetificadora(String sourceDir,FilialScanc filial, String destinationDir, bool recursive)
+        {
+
+            try
+            {
+
+                if (Directory.Exists(sourceDir))
+                {
+                    var dir = new DirectoryInfo(sourceDir);
+
+                    if(dir.Exists)
+                    {
+
+                        DirectoryInfo[] dirs = dir.GetDirectories();
+
+                        Directory.CreateDirectory(destinationDir);
+
+                        foreach (FileInfo file in dir.GetFiles()) {
+
+                            string targetFilePath = Path.Combine(destinationDir, file.Name);
+                            file.CopyTo(targetFilePath);
+
+                            Console.WriteLine("Copiando arquivo" + file.Name);
+
+                        }
+
+                        if (recursive)
+                        {
+                            foreach (DirectoryInfo subDir in dirs)
+                            {
+                                string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+                                CriaRetificadora(subDir.FullName, null, newDestinationDir, true);
+                            }
+                        }
+
+
+                    }
+                }
+
+
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+
+    }
+}
